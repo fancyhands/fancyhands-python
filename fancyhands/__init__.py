c93f93a87f40a3bdea00d85ddfb727a5b3d0ebeb
nmercer <nm3rc3r@gmail.com>
2014-10-29 14:24:50 -0400

    New endpoints for incoming call API

--- fancyhands/__init__.py
+++ fancyhands/__init__.py
@@ -22,8 +22,13 @@ class FancyhandsClient(object):
         if http_method == 'GET':
             url += '?%s' % urlencode(query_params)
             resp, content = client.request(url, http_method)
-        else:
+        elif http_method == 'POST':
             resp, content = client.request(url, http_method, body=urlencode(query_params))
+        elif http_method == 'PUT':
+            resp, content = client.request(url, http_method, body=urlencode(query_params), headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        elif http_method == 'DELETE':
+            url += '?%s' % urlencode(query_params)
+            resp, content = client.request(url, http_method)
 
         try:
             content = json.loads(content)
@@ -33,7 +38,7 @@ class FancyhandsClient(object):
         return content
  
 
-     ##########################################################################################
+    ##########################################################################################
     # Custom API
     ##########################################################################################
 
@@ -81,12 +86,12 @@ class FancyhandsClient(object):
 
 
     ##########################################################################################
-    # Call API
+    # Outgoing API
     ##########################################################################################
 
     # This will allow you to get any call request you have submitted.
-    def call_get(self, key=None, status=None, cursor=None):
-        uri = '/api/v1/call/'
+    def outgoing_get(self, key=None, status=None, cursor=None):
+        uri = '/api/v1/call/outgoing/'
 
         query_params = {
             'key': key,
@@ -101,19 +106,133 @@ class FancyhandsClient(object):
     
     # This will allow you to submit a call task. (phone) is the phone number to be called. (conversation) is the json encoded
     # script for the assistants.
-    def call_create(self, phone=None, conversation=None, retry=False, retry_delay=0, retry_limit=0, test=False):
-        uri = '/api/v1/call/'
+    def outgoing_create(self, phone=None, conversation=None, title=None, record=False, retry=False, retry_delay=None,
+                        retry_limit=None, call_window_start=None, call_window_end=None, test=False):
+        uri = '/api/v1/call/outgoing/'
 
         query_params = {
             'phone': phone,
             'conversation': conversation,
+            'record':record,
             'retry':retry,
             'retry_delay':retry_delay,
             'retry_limit':retry_limit,
+            'title':title,
             'test':test,
         }
 
+        query_params = {i:j for i,j in query_params.items() if j != None}
+
+        if call_window_start and call_window_end:
+            query_params['call_window_start'] = call_window_start.strftime('%Y-%m-%dT%H:%M:%SZ')
+            query_params['call_window_end'] = call_window_end.strftime('%Y-%m-%dT%H:%M:%SZ')
+
         return self.oauth_request(uri=uri, query_params=query_params, http_method='POST')
+
+    ##########################################################################################
+    # Number API
+    ##########################################################################################
+    def number_buy(self, phone_number=None):
+        uri = '/api/v1/call/number/'
+
+        query_params = {
+            'phone_number':phone_number,
+        }
+
+        return self.oauth_request(uri=uri, query_params=query_params, http_method='POST')['phone_numbers']
+
+    def number_get(self, area_code=None, contains=None, region=None):
+        uri = '/api/v1/call/number/'
+
+        query_params = {
+            'area_code':area_code,
+            'contains':contains,
+            'region':region,
+        }
+
+        query_params = {i:j for i,j in query_params.items() if j != None}
+
+        return self.oauth_request(uri=uri, query_params=query_params, http_method='GET')['phone_numbers']
+
+    def number_delete(self, phone_number=None, key=None):
+        uri = '/api/v1/call/number/'
+
+        query_params = {
+            'phone_number':phone_number,
+            'key':key,
+        }
+
+        query_params = {i:j for i,j in query_params.items() if j != None}
+
+        return self.oauth_request(uri=uri, query_params=query_params, http_method='DELETE')
+
+    ##########################################################################################
+    # Incoming API
+    ##########################################################################################
+    def incoming_create(self, phone_number=None, conversation=None):
+        uri = '/api/v1/call/incoming/'
+
+        query_params = {
+            'phone_number':phone_number,
+            'conversation':conversation,
+        }
+
+        return self.oauth_request(uri=uri, query_params=query_params, http_method='POST')['call']
+
+    def incoming_get(self, key=None, phone_number=None, cursor=None):
+        uri = '/api/v1/call/incoming/'
+
+        query_params = {
+            'key':key,
+            'phone_number':phone_number,
+            'cursor':cursor,
+        }
+
+        query_params = {i:j for i,j in query_params.items() if j != None}
+
+        return self.oauth_request(uri=uri, query_params=query_params, http_method='GET')
+
+    def incoming_modify(self, key=None, phone_number=None, conversation=None):
+        uri = '/api/v1/call/incoming/'
+
+        query_params = {
+            'key':key,
+            'phone_number':phone_number,
+            'conversation':conversation,
+        }
+
+        query_params = {i:j for i,j in query_params.items() if j != None}
+
+        return self.oauth_request(uri=uri, query_params=query_params, http_method='PUT')
+
+    def incoming_delete(self, phone_number=None, key=None):
+        uri = '/api/v1/call/incoming/'
+
+        query_params = {
+            'key':key,
+            'phone_number':phone_number,
+        }
+
+        query_params = {i:j for i,j in query_params.items() if j != None}
+
+        return self.oauth_request(uri=uri, query_params=query_params, http_method='DELETE')
+
+    ##########################################################################################
+    # History API
+    ##########################################################################################
+    # Todo - Test
+    def history(self, key=None, phone_number=None, cursor=None):
+        uri = '/api/v1/call/history/'
+
+        query_params = {
+            'key': key,
+            'phone_number': phone_number,
+            'cursor': cursor,
+        }
+        query_params = {i:j for i,j in query_params.items() if j != None}
+
+        return self.oauth_request(uri=uri, query_params=query_params, http_method='GET')
+
 
     ##########################################################################################
     # Standard API
